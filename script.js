73e971a4b44bc28326538c674838e8615e49b1b8
Kimi <samillyaquila@gmail.com>
2024-11-12 16:45:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let variavel1='Olá, mundo';
+let inteiros=82;
+let float=6.8;
+let boleano= true;
+
+
+let apresentacao='olá, meu nome é '
+let nome1='Rebhecca'
+let nome2='Philippe'
+let usuario= 'Áquila'
+
+console.log(apresentacao+nome1)
+console.log(apresentacao+nome2)
+console.log(usuario==='Áquila')
+console.log(10/5)
+
+
+let user='Pedro'
+let senha='pedrinho09'
+
+if(user==='Pedro'&&senha==='pedrinho09'){
+    console.log('Aprovado')
+}else{
+    console.log('Reprovado')
+}
+
+
+
+let bombeiro=false
+let policia=true
+let mãe=true
+
+if(bombeiro===true){
+    console.log('O bombeiro apagou o fogo')
+}else if(policia===true){
+    console.log('O policial apagou o fogo')
+}else if(mãe===true){
+    console.log('Sua mãe te salvou o fogo')
+}else{
+    console.log('Você morreu')
+}
